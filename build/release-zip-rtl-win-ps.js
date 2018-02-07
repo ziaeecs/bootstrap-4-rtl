235b7c752dd139845b9a2c79269820333f81e8eb
Perseus The Great <arash.laylazi@live.com>
2018-02-07 03:55:47 +0330

    Update revision No.: 4

--- build/release-zip-rtl-win-ps.js
+++ build/release-zip-rtl-win-ps.js
@@ -4,9 +4,22 @@
 
 const shell = require('shelljs')
 const packageJson = require('../package.json')
-const cmd = `powershell compress-archive -Path dist/* -CompressionLevel Optimal -DestinationPath bootstrap-${packageJson.version}-plus-rtl-rev.${packageJson['rtl-revision']}-dist.zip`
+const fileName = `bootstrap-${packageJson.version}-plus-rtl-rev.${packageJson['rtl-revision']}-dist.zip`
+const deleteCommand = `PowerShell Remove-Item -Path ${fileName}`
+const buildCommand = `PowerShell Compress-Archive -Path dist/* -CompressionLevel Optimal -DestinationPath ${fileName}`
 
-if (shell.exec(cmd).code !== 0) {
-  shell.echo('Error: Compression Failed!')
-  shell.exit(1)
-}
+shell.exec(deleteCommand, (code, stdout, stderr) => {
+  if (code == 0 || code == 1) {
+    shell.exec(buildCommand, (code, stdout, stderr) => {
+      if (code == 0) {
+        shell.echo('Package build succeeded!')
+      } else {
+        shell.echo(`Error: ${stderr}`)
+        shell.exit(1)
+      }
+    })    
+  } else {
+    shell.echo(`Error: ${stderr}`)
+    shell.exit(1)
+  }
+})
