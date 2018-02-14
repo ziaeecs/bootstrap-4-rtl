eaf3915edbd036b11b61c7c7fca646cbd82283a9
Perseus The Great <arash.laylazi@live.com>
2018-02-14 14:03:03 +0330

    RTL dist, rev.6

--- dist/js/bootstrap.bundle.js
+++ dist/js/bootstrap.bundle.js
@@ -3890,13 +3890,15 @@ var Dropdown = function ($$$1) {
     offset: 0,
     flip: true,
     boundary: 'scrollParent',
-    reference: 'toggle'
+    reference: 'toggle',
+    display: 'dynamic'
   };
   var DefaultType = {
     offset: '(number|string|function)',
     flip: 'boolean',
     boundary: '(string|element)',
-    reference: '(string|element)'
+    reference: '(string|element)',
+    display: 'string'
     /**
      * ------------------------------------------------------------------------
      * Class Definition
@@ -4093,8 +4095,16 @@ var Dropdown = function ($$$1) {
           preventOverflow: {
             boundariesElement: this._config.boundary
           }
-        }
+        } // Disable Popper.js if we have a static display
+
       };
+
+      if (this._config.display === 'static') {
+        popperConfig.modifiers.applyStyle = {
+          enabled: false
+        };
+      }
+
       return popperConfig;
     }; // Static
 
--- dist/js/bootstrap.js
+++ dist/js/bootstrap.js
@@ -1456,13 +1456,15 @@ var Dropdown = function ($$$1) {
     offset: 0,
     flip: true,
     boundary: 'scrollParent',
-    reference: 'toggle'
+    reference: 'toggle',
+    display: 'dynamic'
   };
   var DefaultType = {
     offset: '(number|string|function)',
     flip: 'boolean',
     boundary: '(string|element)',
-    reference: '(string|element)'
+    reference: '(string|element)',
+    display: 'string'
     /**
      * ------------------------------------------------------------------------
      * Class Definition
@@ -1659,8 +1661,16 @@ var Dropdown = function ($$$1) {
           preventOverflow: {
             boundariesElement: this._config.boundary
           }
-        }
+        } // Disable Popper.js if we have a static display
+
       };
+
+      if (this._config.display === 'static') {
+        popperConfig.modifiers.applyStyle = {
+          enabled: false
+        };
+      }
+
       return popperConfig;
     }; // Static
 
