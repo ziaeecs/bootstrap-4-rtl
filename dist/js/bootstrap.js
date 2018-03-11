21af2e8afbdcde70e2927b46f48d836dc0e88ada
Perseus The Great <arash.laylazi@live.com>
2018-03-11 15:40:37 +0330

    RTL dist, rev.8

--- dist/js/bootstrap.js
+++ dist/js/bootstrap.js
@@ -3872,7 +3872,7 @@ var Tab = function ($$$1) {
 
 /**
  * --------------------------------------------------------------------------
- * Bootstrap (v4.0.0-alpha.6): index.js
+ * Bootstrap (v4.0.0): index.js
  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
  * --------------------------------------------------------------------------
  */
