4f62e56b1986775ebc0e6e36df2fd90e7c87c9b8
Perseus The Great <arash.laylazi@live.com>
2018-04-10 18:05:05 +0430

    Sync to official 4.1.0, rev. 1

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 // package metadata file for Meteor.js
 
-/* global Package:true */
+/* eslint-env meteor */
 
 Package.describe({
   name: 'twbs:bootstrap',  // https://atmospherejs.com/twbs/bootstrap
   summary: 'The most popular front-end framework for developing responsive, mobile first projects on the web.',
-  version: '4.0.0',
-  "rtl-revision": '11',
+  version: '4.1.0',
+  "rtl-revision": '1',
   git: 'https://github.com/twbs/bootstrap.git'
 });
 
