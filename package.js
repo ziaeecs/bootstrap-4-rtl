2061e1650f991e848c5e7f4ed0091a87e92e1436
Perseus The Great <arash.laylazi@live.com>
2018-07-30 20:37:55 +0430

    Weekly sync to official ver. 4.1.3, RTL rev. 2

--- package.js
+++ package.js
@@ -6,7 +6,7 @@ Package.describe({
   name: 'twbs:bootstrap',  // https://atmospherejs.com/twbs/bootstrap
   summary: 'The most popular front-end framework for developing responsive, mobile first projects on the web.',
   version: '4.1.3',
-  "rtl-revision": '1',
+  "rtl-revision": '2',
   git: 'https://github.com/twbs/bootstrap.git'
 });
 
